3ddf94f1ce8deb00622d707d6ac8323c01791948
RArielAlonso <rariel.alonso@gmail.com>
2023-05-17 15:30:32 -0300

    initial commit

---
+++ dags/my_dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime
+
+def print_a():
+    print('hi from task a')
+
+def print_b():
+    print('hi from task b')
+
+with DAG(
+    dag_id='my_dag',
+    description="A simple Tutorial DAG",
+    tags=['data_science'],
+    start_date=datetime(2023,5,1),
+    schedule="@daily",
+    catchup=False
+    ) as dag:
+    
+    task_a=PythonOperator(
+        task_id='task_a',
+        python_callable=print_a
+    )
+    task_b=PythonOperator(
+        task_id='task_b',
+        python_callable=print_a
+    )
+
+task_a>>task_b
---
+++ dags/my_dag_with_taskflow_api.py
@@ -0,0 +1,12 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(start_date=datetime(2023, 1, 1), description='A simple tutorial DAG', 
+     tags=['data_science'], schedule='@daily', catchup=False)
+def my_dag_with_taskflow_api():
+    
+    @task
+    def print_a():
+        print('hi from task a')
+
+my_dag_with_taskflow_api()
