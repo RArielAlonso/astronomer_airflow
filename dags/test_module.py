1db90f7e71b00bb82ad90b8375db7bdefbcbcc6c
RArielAlonso <rariel.alonso@gmail.com>
2023-05-29 13:30:32 -0300

    modificacion en custom packages

--- dags/test_module.py
+++ dags/test_module.py
@@ -5,12 +5,11 @@ from airflow.decorators import dag, task
 
 import pendulum
 
-from my_packages.packages_a.module_a import TestClass
+from my_packages.packages_a.module_a.module_a import TestClass
 
 @dag(schedule = None,
      start_date = pendulum.datetime(2023, 3, 1),
      catchup = False)
-
 def test_module():
     @task
     def test_task():
